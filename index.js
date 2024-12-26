ebd5e7875644c97bac6c94a28f5e8a94e789aa90
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-12-26 22:18:55 +0600

    create assignments api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
@@ -14,9 +14,7 @@ app.get("/", (req, res) => {
   res.send("studyHive server running");
 });
 
-
-// mongodb connect 
-
+// mongodb connect
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6oz5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,19 +24,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    
+
+    const assignmentsCollection = client
+      .db("assignmentsDB")
+      .collection("assignments");
+
+    app.get("/allAssignments", async (req, res) => {
+      const cursor = assignmentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/allAssignments", async (req, res) => {
+      const newAssignments = req.body;
+      //console.log(newAssignments);
+      const result = await assignmentsCollection.insertOne(newAssignments);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -46,9 +61,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, (req, res) => {
   console.log(`server running on port: ${port}`);
 });
-
-
